5b6033ecf940d9b53dcd3645167d0b6f8ab9c5cc
Ayush <ayush@indimedi.in>
2024-06-09 23:53:52 +0530

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { ChangeDetectionStrategy } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    FormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule ,
+    BrowserAnimationsModule,
+   ],
   providers: [],
   bootstrap: [AppComponent]
 })
