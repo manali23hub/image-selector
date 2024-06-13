89cf5f31e9b8281583e014e39a99eb9d5e34b61b
manali23hub <58810247+manali23hub@users.noreply.github.com>
2024-06-13 20:12:19 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component , VERSION, OnInit} from '@angular/core';
+//importing json file 
 import * as Data from '../assets/image.json';
 
 @Component({
@@ -6,35 +7,40 @@ import * as Data from '../assets/image.json';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent  implements OnInit {
-  dropdown_level: any;
+export class AppComponent {
+
   title = 'image-selector';
+    //initializing variables
+  dropdown_level: any;
   imgSelectArr: any[] = [];
   count: any = 0;
   img_list = [5,10,15,20];
   selectedValue: any;
   imageArr: any = Data['sample'];
-  ngOnInit(): void {
-    console.log('oninit');
-  }
+  //Image click function
   imageClicked(img: any) {
     this.count = this.count + 1;
+    //Setting boolean field found_image to find out image is selected or de-selected
     const found_image = this.imgSelectArr.some((el: any) => el.id === img.id);
-    if (!found_image) {
-      img.isChecked = true;
-      img.count = this.count;
-      this.imgSelectArr.push(img);
-    }
+    
     if (found_image) {
+      //setting count to 0 as image is de-selected
       img.count = 0;
-      this.imgSelectArr.splice(
-        this.imgSelectArr.findIndex((a) => a.id === img.id),1);
+      //find index of the image which is deselected using findIndex function 
+      //and removing it from array using splice function .
+      this.imgSelectArr.splice(this.imgSelectArr.findIndex((a) => a.id === img.id),1);
+      //updating count
       this.count = 0;
       this.imgSelectArr.forEach((element) => {
         this.count = this.count + 1;
         element.count = this.count;
       });
-    
+    }
+    else if (!found_image) {
+      img.isChecked = true;
+      img.count = this.count;
+      //Pushing image in the array
+      this.imgSelectArr.push(img);
     }
 
   }
